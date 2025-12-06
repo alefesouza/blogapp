616bbbecc3c976c23f7783260b4193e261927e97
Alefe Souza <contact@alefesouza.com>
2025-12-06 13:48:59 -0300

    Code for version 2.1.1, launched on 04/12/2014

--- CasaDoCogumelo/src/net/aloogle/acasadocogumelo/receiver/NotificationReceiver.java
+++ CasaDoCogumelo/src/net/aloogle/acasadocogumelo/receiver/NotificationReceiver.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.BigTextStyle;
 import android.support.v4.app.TaskStackBuilder;
@@ -22,39 +23,50 @@ public class NotificationReceiver extends BroadcastReceiver {
 			String action = intent.getAction();
 			if (action.equals("net.aloogle.acasadocogumelo.UPDATE_STATUS")) {
 				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+				Editor editor = preferences.edit();
 				boolean notification = preferences.getBoolean("prefNotification", true);
 				if (notification) {
 					JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
 
-					NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-						.setSmallIcon(R.drawable.ic_launcher)
-						.setTicker(json.getString("barra"))
-						.setContentTitle(json.getString("titulo"))
-						.setContentText(json.getString("texto"))
-						.setAutoCancel(true)
-						.setLights(0xFFFF0000, 1500, 2500)
-						.setDefaults(NotificationCompat.DEFAULT_SOUND | NotificationCompat.DEFAULT_VIBRATE)
-						.setStyle(new BigTextStyle()
-							.bigText(json.getString("textogrande"))
-							.setBigContentTitle(json.getString("titulogrande"))
-							.setSummaryText(json.getString("sumario")));
+					if (json.getString("id").equals(preferences.getString("lastPostId", "5"))) {}
+					else {
+						if (json.getString("titulo").equals("")) {}
+						else {
+							editor.putString("lastPostId", json.getString("id"));
+							editor.commit();
 
-					Intent resultIntent = new Intent(context, net.aloogle.acasadocogumelo.activity.SplashScreen.class);
-					resultIntent.putExtra("url", json.getString("url"));
+							NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+								.setSmallIcon(R.drawable.ic_launcher)
+								.setTicker(json.getString("barra"))
+								.setContentTitle(json.getString("titulo"))
+								.setContentText(json.getString("texto"))
+								.setAutoCancel(true)
+								.setSound(Uri.parse("android.resource://net.aloogle.acasadocogumelo/raw/ringtone"))
+								.setLights(0xFFFF0000, 1500, 2500)
+								.setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+								.setStyle(new BigTextStyle()
+									.bigText(json.getString("textogrande"))
+									.setBigContentTitle(json.getString("titulogrande"))
+									.setSummaryText(json.getString("sumario")));
 
-					TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+							editor.putInt("notifID", preferences.getInt("notifID", 0) + 1);
+							editor.commit();
 
-					stackBuilder.addParentStack(net.aloogle.acasadocogumelo.activity.SplashScreen.class);
+							Intent resultIntent = new Intent(context, net.aloogle.acasadocogumelo.activity.SplashScreen.class);
+							resultIntent.putExtra("url", json.getString("url"));
 
-					stackBuilder.addNextIntent(resultIntent);
-					PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-					mBuilder.setContentIntent(resultPendingIntent);
-					NotificationManager mNotificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-					Editor editor = preferences.edit();
-					editor.putInt("notifID", preferences.getInt("notifID", 0) + 1);
-					editor.commit();
+							TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
 
-					mNotificationManager.notify(preferences.getInt("notifID", 0), mBuilder.build());
+							stackBuilder.addParentStack(net.aloogle.acasadocogumelo.activity.SplashScreen.class);
+
+							stackBuilder.addNextIntent(resultIntent);
+							PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(preferences.getInt("notifID", 0), PendingIntent.FLAG_UPDATE_CURRENT);
+							mBuilder.setContentIntent(resultPendingIntent);
+							NotificationManager mNotificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+							mNotificationManager.notify(preferences.getInt("notifID", 0), mBuilder.build());
+						}
+					}
 				}
 			}
 		} catch (JSONException e) {}
