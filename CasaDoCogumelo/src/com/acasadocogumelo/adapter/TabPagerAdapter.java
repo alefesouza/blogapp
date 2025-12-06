f0a693c01e03d0757f3540c4f7dee4ba2c791247
Alefe Souza <contact@alefesouza.com>
2025-12-06 14:40:11 -0300

    Code for version 3.1, launched on 09/05/2015

---
+++ CasaDoCogumelo/src/com/acasadocogumelo/adapter/TabPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.acasadocogumelo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import com.acasadocogumelo.fragment.MainFragment;
+import com.acasadocogumelo.fragment.PopularFragment;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+	public TabPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		String title = null;
+		if (position == 0) {
+			title = "Recentes";
+		} else if (position == 1) {
+			title = "Populares";
+		}
+		return title;
+	}
+
+	@Override
+	public Fragment getItem(int i) {
+		switch (i) {
+		case 0:
+			return new MainFragment();
+		case 1:
+			return new PopularFragment();
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		return 2;
+	}
+}
---
+++ CasaDoCogumelo/src/com/acasadocogumelo/fragment/CommentsFragment.java
@@ -0,0 +1,289 @@
+package com.acasadocogumelo.fragment;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+import android.graphics.LightingColorFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebChromeClient.CustomViewCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import java.lang.reflect.InvocationTargetException;
+import com.gc.materialdesign.views.ProgressBarCircularIndeterminate;
+import com.gc.materialdesign.views.ProgressBarDeterminate;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ObservableWebView;
+
+import com.acasadocogumelo.R;
+
+@SuppressLint({ "NewApi", "SetJavaScriptEnabled" })
+public class CommentsFragment extends Fragment {
+	View view;
+	Activity activity;
+
+	SharedPreferences preferences;
+	Editor editor;
+	String iconcolor;
+	ObservableWebView webView;
+	ProgressBar progressBar;
+	ProgressBarCircularIndeterminate progressBarCompat;
+	ProgressBarDeterminate progressBar2;
+	boolean finished;
+
+	long enqueue;
+
+	private FrameLayout mTargetView;
+	private FrameLayout mContentView;
+	private CustomViewCallback mCustomViewCallback;
+	private View mCustomView;
+	private webChromeClient mClient;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		this.activity = getActivity();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		iconcolor = preferences.getString("prefIconColor", "branco");
+		view = inflater.inflate(R.layout.webview, container, false);
+
+		finished = false;
+
+		progressBar2 = (ProgressBarDeterminate)view.findViewById(R.id.progressBar2);
+
+		if(Build.VERSION.SDK_INT >= 21) {
+			progressBar = (ProgressBar)view.findViewById(R.id.progressBar1);
+			progressBar.getIndeterminateDrawable().setColorFilter(new LightingColorFilter(0xFFD92525, 0xFFD92525));
+		} else {
+			progressBarCompat = (ProgressBarCircularIndeterminate)view.findViewById(R.id.progressBar1);
+		}
+
+		webView = (ObservableWebView)view.findViewById(R.id.webview01);
+		if(Build.VERSION.SDK_INT > 10) {
+			webView.setTouchInterceptionViewGroup((ViewGroup)getActivity().findViewById(R.id.container));
+			webView.setScrollViewCallbacks((ObservableScrollViewCallbacks)getActivity());
+		}
+		mClient = new webChromeClient();
+		webView.setWebChromeClient(mClient);
+		webView.setWebViewClient(new webViewClient());
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setSupportZoom(true);
+		webView.getSettings().setBuiltInZoomControls(true);
+		if (Build.VERSION.SDK_INT >= 11) {
+			webView.getSettings().setDisplayZoomControls(false);
+		}
+
+		if (savedInstanceState != null) {
+			webView.restoreState(savedInstanceState);
+		} else {
+			webView.loadUrl(getArguments().getString("url"));
+		}
+
+		webView.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(final String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				startActivity(i);
+			}
+		});
+
+		mContentView = (FrameLayout)view.findViewById(R.id.main_content);
+		mTargetView = (FrameLayout)view.findViewById(R.id.target_view);
+		webView.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(final String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				startActivity(i);
+			}
+		});
+
+		return view;
+	}
+
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		if (webView.canGoBack()) {
+			menu.findItem(R.id.menu_back).setEnabled(true);
+		} else {
+			menu.findItem(R.id.menu_back).setEnabled(false);
+		}
+
+		if (webView.canGoForward()) {
+			menu.findItem(R.id.menu_forward).setEnabled(true);
+		} else {
+			menu.findItem(R.id.menu_forward).setEnabled(false);
+		}
+
+		menu.findItem(R.id.menu_share).setVisible(false);
+		super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_back:
+			webView.goBack();
+			return true;
+		case R.id.menu_forward:
+			webView.goForward();
+			return true;
+		case R.id.menu_reload:
+			webView.reload();
+			return true;
+		case R.id.menu_share:
+			Intent sharePageIntent = new Intent();
+			sharePageIntent.setAction(Intent.ACTION_SEND);
+			sharePageIntent.putExtra(Intent.EXTRA_TEXT, webView.getTitle() + " " + webView.getUrl().replace("?m=1", ""));
+			sharePageIntent.setType("text/plain");
+			startActivity(Intent.createChooser(sharePageIntent, getResources().getText(R.string.share)));
+			return true;
+		case R.id.menu_cache:
+			webView.clearCache(true);
+			Toast toast = Toast.makeText(getActivity(), getString(R.string.cache2), Toast.LENGTH_LONG);
+			toast.show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public class webViewClient extends WebViewClient {
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			super.onPageStarted(view, url, favicon);
+		}
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (finished == false) {
+				if (url.contains("acasadocogumelo.com") || url.contains("facebook.com") || url.contains("google.com") || url.contains("twitter.com") || url.contains("youtube.com") || url.contains(".jpg") || url.contains(".png") || url.contains(".gif")) {
+					view.loadUrl(url);
+				} else {
+					Intent i = new Intent(Intent.ACTION_VIEW);
+					i.setData(Uri.parse(url));
+					startActivity(i);
+				}
+				getActivity().supportInvalidateOptionsMenu();
+			}
+			return true;
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+		}
+
+		@Override
+		public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+			String erro = "<html><head><style>body { background-image: url('erro.png'); background-repeat: no-repeat; background-position: center; background-color: #000000; min-height: 431px; }</style></head></html>";
+			webView.loadDataWithBaseURL("file:///android_asset/", erro, "text/html", "utf-8", null);
+			super.onReceivedError(view, errorCode, description, failingUrl);
+		}
+	}
+
+	public class webChromeClient extends WebChromeClient {
+		public void onProgressChanged(WebView view, int progress) {
+			if (finished == false) {
+				progressBar2.setProgress(progress);
+				progressBar2.setVisibility(View.VISIBLE);
+
+				if (progress == 100) {
+					progressBar2.setVisibility(View.GONE);
+				}
+
+				if (progress >= 50) {
+					if(Build.VERSION.SDK_INT >= 21) {
+						progressBar.setVisibility(View.GONE);
+					} else {
+						progressBarCompat.setVisibility(View.GONE);
+					}
+					webView.setVisibility(View.VISIBLE);
+				}
+			}
+		}
+
+		@Override
+		public void onShowCustomView(View view, CustomViewCallback callback) {
+			mCustomViewCallback = callback;
+			mTargetView.addView(view);
+			mCustomView = view;
+			mContentView.setVisibility(View.GONE);
+			mTargetView.setVisibility(View.VISIBLE);
+			mTargetView.bringToFront();
+			((ActionBarActivity)getActivity()).getSupportActionBar().hide();
+		}
+
+		@Override
+		public void onHideCustomView() {
+			if (mCustomView == null)
+				return;
+
+			mCustomView.setVisibility(View.GONE);
+			mTargetView.removeView(mCustomView);
+			mCustomView = null;
+			mTargetView.setVisibility(View.GONE);
+			mCustomViewCallback.onCustomViewHidden();
+			mContentView.setVisibility(View.VISIBLE);
+			((ActionBarActivity)getActivity()).getSupportActionBar().show();
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		try {
+			Class.forName
+			("android.webkit.WebView")
+			.getMethod
+			("onPause", (Class[])null)
+			.invoke
+			(webView, (Object[])null);
+		} catch (ClassNotFoundException cnfe) {}
+
+		catch (NoSuchMethodException nsme) {}
+
+		catch (InvocationTargetException ite) {}
+
+		catch (IllegalAccessException iae) {}
+	}
+
+	@Override
+	public void onDestroy() {
+		finished = true;
+		super.onDestroy();
+	}
+
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		webView.saveState(savedInstanceState);
+		super.onSaveInstanceState(savedInstanceState);
+	}
+}
